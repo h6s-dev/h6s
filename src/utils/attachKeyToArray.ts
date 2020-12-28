7abeb2f0f560ae70c05a05778438b4a2074cb6cf
JaeYeopHan <ljyhanll@gmail.com>
2020-12-28 15:33:34 +0900

    fix: add generic to util function

--- src/utils/attachKeyToArray.ts
+++ src/utils/attachKeyToArray.ts
@@ -1,6 +1,6 @@
 import generateID from './generateID'
 
-export default function attachKeyToArray(arr: unknown[], keyPrefix: string) {
+export default function attachKeyToArray<T>(arr: T[], keyPrefix: string) {
   return arr.map(value => {
     return {
       key: generateID(keyPrefix),
