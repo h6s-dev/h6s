234bd7df103ad8245846cec5502fe96a222b8d51
JaeYeopHan <ljyhanll@gmail.com>
2020-12-29 17:48:41 +0900

    feat(util): add pipe function

---
+++ src/utils/pipe.test.ts
@@ -0,0 +1,167 @@
+import { pipe, pipeWith } from './pipe'
+
+const zeroParamGetNumber = () => 1
+const singleParamFnAdd1 = (n: number) => n + 1
+const singleParamFnTimes2 = (n: number) => n * 2
+const multipleParamFnDifference = (a: number, b: number) => a - b
+
+describe('pipe function', () => {
+  it('works when first function has 0 params', () => {
+    const result = pipe(zeroParamGetNumber, singleParamFnAdd1)()
+    expect(result).toEqual(2)
+  })
+
+  it('works when first function has single param', () => {
+    expect(pipe(singleParamFnAdd1)(1)).toEqual(2)
+    expect(pipe(singleParamFnAdd1, singleParamFnTimes2)(1)).toEqual(4)
+    expect(
+      pipe(singleParamFnAdd1, singleParamFnTimes2, singleParamFnAdd1)(1),
+    ).toEqual(5)
+    expect(
+      pipe(
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+      )(1),
+    ).toEqual(10)
+    expect(
+      pipe(
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+      )(1),
+    ).toEqual(11)
+    expect(
+      pipe(
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+      )(1),
+    ).toEqual(22)
+    expect(
+      pipe(
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+      )(1),
+    ).toEqual(23)
+    expect(
+      pipe(
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+      )(1),
+    ).toEqual(46)
+    expect(
+      pipe(
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+      )(1),
+    ).toEqual(47)
+  })
+
+  it('works when first function has multiple params', () => {
+    expect(pipe(multipleParamFnDifference, singleParamFnAdd1)(5, 4)).toEqual(2)
+  })
+})
+
+describe(pipeWith.name, () => {
+  it('works', () => {
+    expect(pipeWith(1, singleParamFnAdd1)).toEqual(2)
+    expect(pipeWith(1, singleParamFnAdd1, singleParamFnTimes2)).toEqual(4)
+    expect(
+      pipeWith(1, singleParamFnAdd1, singleParamFnTimes2, singleParamFnAdd1),
+    ).toEqual(5)
+    expect(
+      pipeWith(
+        1,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+      ),
+    ).toEqual(10)
+    expect(
+      pipeWith(
+        1,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+      ),
+    ).toEqual(11)
+    expect(
+      pipeWith(
+        1,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+      ),
+    ).toEqual(22)
+    expect(
+      pipeWith(
+        1,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+      ),
+    ).toEqual(23)
+    expect(
+      pipeWith(
+        1,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+      ),
+    ).toEqual(46)
+    expect(
+      pipeWith(
+        1,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+        singleParamFnTimes2,
+        singleParamFnAdd1,
+      ),
+    ).toEqual(47)
+  })
+})
---
+++ src/utils/pipe.ts
@@ -0,0 +1,152 @@
+type UnknownFunction = (...params: unknown[]) => unknown
+
+// Copied from https://github.com/gcanti/fp-ts/blob/1.15.0/src/function.ts#L209 with some
+// modifications to make the first function allow multiple parameters.
+export function pipe<A extends unknown[], B>(
+  ab: (this: void, ...a: A) => B,
+): (...args: A) => B
+export function pipe<A extends unknown[], B, C>(
+  ab: (this: void, ...a: A) => B,
+  bc: (this: void, b: B) => C,
+): (...args: A) => C
+export function pipe<A extends unknown[], B, C, D>(
+  ab: (this: void, ...a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+): (...args: A) => D
+export function pipe<A extends unknown[], B, C, D, E>(
+  ab: (this: void, ...a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+  de: (this: void, d: D) => E,
+): (...args: A) => E
+export function pipe<A extends unknown[], B, C, D, E, F>(
+  ab: (this: void, ...a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+  de: (this: void, d: D) => E,
+  ef: (this: void, e: E) => F,
+): (...args: A) => F
+export function pipe<A extends unknown[], B, C, D, E, F, G>(
+  ab: (this: void, ...a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+  de: (this: void, d: D) => E,
+  ef: (this: void, e: E) => F,
+  fg: (this: void, f: F) => G,
+): (...args: A) => G
+export function pipe<A extends unknown[], B, C, D, E, F, G, H>(
+  ab: (this: void, ...a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+  de: (this: void, d: D) => E,
+  ef: (this: void, e: E) => F,
+  fg: (this: void, f: F) => G,
+  gh: (this: void, g: G) => H,
+): (...args: A) => H
+export function pipe<A extends unknown[], B, C, D, E, F, G, H, I>(
+  ab: (this: void, ...a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+  de: (this: void, d: D) => E,
+  ef: (this: void, e: E) => F,
+  fg: (this: void, f: F) => G,
+  gh: (this: void, g: G) => H,
+  hi: (this: void, h: H) => I,
+): (...args: A) => I
+export function pipe<A extends unknown[], B, C, D, E, F, G, H, I, J>(
+  ab: (this: void, ...a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+  de: (this: void, d: D) => E,
+  ef: (this: void, e: E) => F,
+  fg: (this: void, f: F) => G,
+  gh: (this: void, g: G) => H,
+  hi: (this: void, h: H) => I,
+  ij: (this: void, i: I) => J,
+): (...args: A) => J
+export function pipe(...fns: UnknownFunction[]): UnknownFunction {
+  return (...initialParams: unknown[]): unknown =>
+    fns.reduce<unknown>((value, fn, index) => {
+      const params = index === 0 ? (value as unknown[]) : [value]
+
+      return fn(...params)
+    }, initialParams)
+}
+
+export function pipeWith<A, B>(a: A, ab: (this: void, a: A) => B): B
+export function pipeWith<A, B, C>(
+  a: A,
+  ab: (this: void, a: A) => B,
+  bc: (this: void, b: B) => C,
+): C
+export function pipeWith<A, B, C, D>(
+  a: A,
+  ab: (this: void, a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+): D
+export function pipeWith<A, B, C, D, E>(
+  a: A,
+  ab: (this: void, a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+  de: (this: void, d: D) => E,
+): E
+export function pipeWith<A, B, C, D, E, F>(
+  a: A,
+  ab: (this: void, a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+  de: (this: void, d: D) => E,
+  ef: (this: void, e: E) => F,
+): F
+export function pipeWith<A, B, C, D, E, F, G>(
+  a: A,
+  ab: (this: void, a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+  de: (this: void, d: D) => E,
+  ef: (this: void, e: E) => F,
+  fg: (this: void, f: F) => G,
+): G
+export function pipeWith<A, B, C, D, E, F, G, H>(
+  a: A,
+  ab: (this: void, a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+  de: (this: void, d: D) => E,
+  ef: (this: void, e: E) => F,
+  fg: (this: void, f: F) => G,
+  gh: (this: void, g: G) => H,
+): H
+export function pipeWith<A, B, C, D, E, F, G, H, I>(
+  a: A,
+  ab: (this: void, a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+  de: (this: void, d: D) => E,
+  ef: (this: void, e: E) => F,
+  fg: (this: void, f: F) => G,
+  gh: (this: void, g: G) => H,
+  hi: (this: void, h: H) => I,
+): I
+export function pipeWith<A, B, C, D, E, F, G, H, I, J>(
+  a: A,
+  ab: (this: void, a: A) => B,
+  bc: (this: void, b: B) => C,
+  cd: (this: void, c: C) => D,
+  de: (this: void, d: D) => E,
+  ef: (this: void, e: E) => F,
+  fg: (this: void, f: F) => G,
+  gh: (this: void, g: G) => H,
+  hi: (this: void, h: H) => I,
+  ij: (this: void, i: I) => J,
+): J
+export function pipeWith(value: unknown, ...fns: UnknownFunction[]): unknown {
+  return pipe(
+    // Our overloads do not currently support arrays
+    // @ts-ignore
+    ...fns,
+  )(value)
+}
