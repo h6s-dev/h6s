c5f3802aa30f06229b3dfd7bcfa1cb547c94722c
JaeYeopHan <ljyhanll@gmail.com>
2020-12-31 17:08:38 +0900

    chore(core): add mock data

---
+++ src/mocks/mockDate.ts
@@ -0,0 +1,71 @@
+export const DecemberMonthMatrix = {
+  value: [
+    {
+      value: [
+        { value: new Date(2020, 10, 29) },
+        { value: new Date(2020, 10, 30) },
+        { value: new Date(2020, 11, 1) },
+        { value: new Date(2020, 11, 2) },
+        { value: new Date(2020, 11, 3) },
+        { value: new Date(2020, 11, 4) },
+        { value: new Date(2020, 11, 5) },
+      ],
+    },
+    {
+      value: [
+        { value: new Date(2020, 11, 6) },
+        { value: new Date(2020, 11, 7) },
+        { value: new Date(2020, 11, 8) },
+        { value: new Date(2020, 11, 9) },
+        { value: new Date(2020, 11, 10) },
+        { value: new Date(2020, 11, 11) },
+        { value: new Date(2020, 11, 12) },
+      ],
+    },
+    {
+      value: [
+        { value: new Date(2020, 11, 13) },
+        { value: new Date(2020, 11, 14) },
+        { value: new Date(2020, 11, 15) },
+        { value: new Date(2020, 11, 16) },
+        { value: new Date(2020, 11, 17) },
+        { value: new Date(2020, 11, 18) },
+        { value: new Date(2020, 11, 19) },
+      ],
+    },
+    {
+      value: [
+        { value: new Date(2020, 11, 20) },
+        { value: new Date(2020, 11, 21) },
+        { value: new Date(2020, 11, 22) },
+        { value: new Date(2020, 11, 23) },
+        { value: new Date(2020, 11, 24) },
+        { value: new Date(2020, 11, 25) },
+        { value: new Date(2020, 11, 26) },
+      ],
+    },
+    {
+      value: [
+        { value: new Date(2020, 11, 27) },
+        { value: new Date(2020, 11, 28) },
+        { value: new Date(2020, 11, 29) },
+        { value: new Date(2020, 11, 30) },
+        { value: new Date(2020, 11, 31) },
+        { value: new Date(2020, 12, 1) },
+        { value: new Date(2020, 12, 2) },
+      ],
+    },
+  ],
+}
+
+export const DecemberFirstWeekData = {
+  value: [
+    { value: new Date(2020, 10, 29) },
+    { value: new Date(2020, 10, 30) },
+    { value: new Date(2020, 11, 1) },
+    { value: new Date(2020, 11, 2) },
+    { value: new Date(2020, 11, 3) },
+    { value: new Date(2020, 11, 4) },
+    { value: new Date(2020, 11, 5) },
+  ],
+}
