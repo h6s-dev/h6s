96dc92d18063c0bc2780df91a74b4604e0efe815
JaeYeopHan <ljyhanll@gmail.com>
2021-03-31 17:40:46 +0900

    chore: add test

--- src/utils/parseDate.test.ts
+++ src/utils/parseDate.test.ts
@@ -3,12 +3,16 @@ import parseDate from './parseDate'
 describe('parseDate util function', () => {
   it('return correct year, month, day', () => {
     // Given
-    const date = new Date('2020-12-27')
     // When
-    const result = parseDate(date)
+    const result1 = parseDate(new Date('2020-12-27'))
+    const result2 = parseDate(new Date(2020, 11, 27))
+
     // Then
-    expect(result.year).toBe(2020)
-    expect(result.month).toBe(11)
-    expect(result.day).toBe(27)
+    expect(result1.year).toBe(2020)
+    expect(result1.month).toBe(11)
+    expect(result1.day).toBe(27)
+    expect(result2.year).toBe(2020)
+    expect(result2.month).toBe(11)
+    expect(result2.day).toBe(27)
   })
 })
