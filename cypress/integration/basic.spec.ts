bdc5d77963e9be56f86ab6d2beba0280e678df25
JaeYeopHan <ljyhanll@gmail.com>
2021-05-09 17:41:37 +0900

    fix: add basic test case

--- cypress/integration/basic.spec.ts
+++ cypress/integration/basic.spec.ts
@@ -1,12 +1,16 @@
+import { format } from 'date-fns'
+
 const BASIC_EXAMPLES_URL =
   'https://veccu-react-calendar-examples-basic.netlify.app'
 
 describe('@veccu/reactl-calendar basic examples e2e test', () => {
   beforeEach(() => {
     cy.visit(BASIC_EXAMPLES_URL)
+    cy.log(`[E2E] Run on ${format(new Date(), 'yyyy-MM-dd (E) HH:mm')}`)
   })
 
-  it('run', () => {
-    cy.log(`[E2E] Run on ${Date.now()}`)
+  it('run navigate successfully', () => {
+    cy.findByLabelText('prev-button').click()
+    cy.findByLabelText('next-button').click()
   })
 })
