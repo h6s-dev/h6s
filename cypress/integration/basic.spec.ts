a5dcfa9483e83932a2ad91a0df09c9d329f571d6
Jbee <ljyhanll@gmail.com>
2021-05-09 18:57:19 +0900

    chore: e2e test code (#29)

--- cypress/integration/basic.spec.ts
+++ cypress/integration/basic.spec.ts
@@ -1,16 +1,91 @@
-import { format } from 'date-fns'
+import {
+  format,
+  getDate,
+  getDaysInMonth,
+  getMonth,
+  getWeeksInMonth,
+  getYear,
+} from 'date-fns'
 
 const BASIC_EXAMPLES_URL =
   'https://veccu-react-calendar-examples-basic.netlify.app'
 
-describe('@veccu/reactl-calendar basic examples e2e test', () => {
+const baseDate = new Date()
+const baseYear = getYear(baseDate)
+const baseMonth = getMonth(baseDate) + 1
+const baseDay = getDate(baseDate)
+const baseDays = getDaysInMonth(baseDate)
+const baseCalendarInfo = {
+  weeksLength: getWeeksInMonth(baseDate),
+}
+
+describe('@veccu/reactl-calendar - basic examples e2e test with default options', () => {
   beforeEach(() => {
     cy.visit(BASIC_EXAMPLES_URL)
-    cy.log(`[E2E] Run on ${format(new Date(), 'yyyy-MM-dd (E) HH:mm')}`)
+    cy.log(`[E2E] Run on ${format(baseDate, 'yyyy-MM-dd (E) HH:mm')}`)
+  })
+
+  it('render calendar successfully', () => {
+    // render today in title
+    cy.findByTestId('cursor-date')
+      .should('include.text', baseYear)
+      .should('include.text', baseMonth)
+    // render weekends in calendar
+    cy.findAllByTestId('calendar-weekends').should('have.length', 7)
+    // render weeks in calendar
+    cy.findAllByTestId('calendar-weeks').should(
+      'have.length',
+      baseCalendarInfo.weeksLength,
+    )
+    // render calendar cells
+    cy.findAllByTestId('calendar-cell').should(
+      'have.length.at.least',
+      baseDays - 1, // sub today
+    )
+    // render today
+    cy.findByTestId('calendar-cell--today').should('include.text', baseDay)
   })
 
   it('run navigate successfully', () => {
-    cy.findByLabelText('prev-button').click()
-    cy.findByLabelText('next-button').click()
+    cy.findByLabelText('button for navigating to prev calendar').click()
+    cy.findByTestId('cursor-date')
+      .should('include.text', baseMonth === 1 ? baseYear - 1 : baseYear)
+      .should('include.text', baseMonth - 1)
+
+    cy.findByLabelText('button for navigating to prev calendar').click()
+    cy.findByTestId('cursor-date')
+      .should('include.text', baseMonth === 1 ? baseYear - 1 : baseYear)
+      .should('include.text', baseMonth - 2)
+
+    cy.findByLabelText('button for navigating to today calendar').click()
+    cy.findByTestId('cursor-date')
+      .should('include.text', baseYear)
+      .should('include.text', baseMonth)
+
+    cy.findByLabelText('button for navigating to next calendar').click()
+    cy.findByTestId('cursor-date')
+      .should('include.text', baseMonth === 12 ? baseYear + 1 : baseYear)
+      .should('include.text', baseMonth + 1)
+
+    cy.findByLabelText('button for navigating to next calendar').click()
+    cy.findByTestId('cursor-date')
+      .should('include.text', baseMonth === 12 ? baseYear + 1 : baseYear)
+      .should('include.text', baseMonth + 2)
+
+    cy.findByLabelText('button for navigating to today calendar').click()
+    cy.findByTestId('cursor-date')
+      .should('include.text', baseYear)
+      .should('include.text', baseMonth)
+  })
+
+  it('run change view type successfully', () => {
+    cy.findByLabelText('button for changing view type to month').click()
+
+    cy.findByLabelText('button for changing view type to week').click()
+    cy.findAllByTestId('calendar-weeks').should('have.length', 1)
+
+    cy.findByLabelText('button for changing view type to day').click()
+    cy.findAllByTestId('calendar-weeks').should('have.length', 1)
+    cy.findByTestId('calendar-cell--today').should('include.text', baseDay)
   })
 })
