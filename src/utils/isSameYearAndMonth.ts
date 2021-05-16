2dd69506f033ce6d90ee555016ba6af26bce584a
bugtype <bugtype.kr@gmail.com>
2021-05-16 14:10:07 +0900

    refactor(utils): isSameMonth naming and importing code at test file

--- src/utils/isSameYearAndMonth.ts
+++ src/utils/isSameYearAndMonth.ts
@@ -1,5 +1,5 @@
 import { isSameMonth as _isSameMonth, isSameYear } from 'date-fns'
 
-export default function isSameMonth(baseDate: Date, targetDate: Date) {
+export default function isSameYearAndMonth(baseDate: Date, targetDate: Date) {
   return _isSameMonth(targetDate, baseDate) && isSameYear(targetDate, baseDate)
 }
