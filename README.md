e017a97dde66ba1a4d0cf6d5a1dcf726976c64a4
JaeYeopHan <ljyhanll@gmail.com>
2021-12-30 21:56:17 +0900

    chore: add assets

--- README.md
+++ README.md
@@ -1,25 +1,12 @@
-# h6s means headless
+![logo](./assets/logo.png)
+
+# h6s
 
 [![GitHub Action Release Status](https://github.com/veccu/react-calendar/workflows/Release/badge.svg)](https://github.com/veccu/react-calendar/actions/workflows/release.yml) [![GitHub Action CI Status](https://github.com/veccu/react-calendar/workflows/CI/badge.svg)](https://github.com/veccu/react-calendar/actions/workflows/ci.yml) [![GitHub Action E2E Status](https://github.com/veccu/react-calendar/workflows/e2e/badge.svg)](https://github.com/veccu/react-calendar/actions/workflows/e2e.yml) [![GitHub Action Sanity Status](https://github.com/veccu/react-calendar/workflows/sanity/badge.svg)](https://github.com/veccu/react-calendar/actions/workflows/sanity.yml)
 
 [![semantic-release](https://img.shields.io/badge/%20%20%F0%9F%93%A6%F0%9F%9A%80-semantic--release-e10079.svg)](https://github.com/semantic-release/semantic-release) [![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/) [![npm version](https://badge.fury.io/js/%40veccu%2Freact-calendar.svg)](https://badge.fury.io/js/%40veccu%2Freact-calendar) [![gzipped size](https://badgen.net/bundlephobia/minzip/%40veccu%2Freact-calendar@latest)](https://bundlephobia.com/result?p=@veccu/react-calendar@latest) <!-- ALL-CONTRIBUTORS-BADGE:START - Do not remove or modify this section -->
 [![All Contributors](https://img.shields.io/badge/all_contributors-6-orange.svg?style=flat-square)](#contributors-)
 <!-- ALL-CONTRIBUTORS-BADGE:END -->
-
-## Come to [@veccu/react-calendar docs](https://veccu-react-calendar.netlify.app/)
-
-## Quickly
-
-For Calendar,
-
-- 🧙‍♂️ Headless
-- 🔖 Lightweight
-- 🏞 Support view type (monthly, weekly, daily)
-- 🕹 Control with navigation
-
-### Examples
-
-- [Basic](https://veccu-react-calendar-examples-basic.netlify.app/)
 
 ## Contributors ✨
 
--- website/src/components/HomepageHeader.tsx
+++ website/src/components/HomepageHeader.tsx
@@ -12,31 +12,34 @@ export function HomepageHeader() {
 
   return (
     <header className={clsx('hero', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title" style={{ maxWidth: 620 }}>
-          {siteConfig.tagline}
-        </h1>
-        <p className="hero__subtitle">
-          <strong className={styles.gradientRedOrange}>{siteConfig.title}</strong>{' '}
-          means{' '}
-          <span ref={ref} className={styles.animationFlutter}>
-            {isHover
-              ? <strong className={clsx('reverse', styles.gradientOrangeRed)}>headless</strong>
-              : <span className={styles.gradientSlates}>h______s</span>}
-          </span>
-        </p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--primary button--lg"
-            to="/docs/intro">
-            Get Started
-          </Link>
-          <Link
-            className="button button--primary button--outline button--lg"
-            to="/docs/intro">
-            GitHub
-          </Link>
+      <div className={clsx(['container', styles.flex, styles.spaceBetween])}>
+        <div>
+          <h1 className="hero__title" style={{ maxWidth: 620 }}>
+            {siteConfig.tagline}
+          </h1>
+          <p className="hero__subtitle">
+            <strong className={styles.gradientRedOrange}>{siteConfig.title}</strong>{' '}
+            means{' '}
+            <span ref={ref} className={styles.animationFlutter}>
+              {isHover
+                ? <strong className={clsx('reverse', styles.gradientOrangeRed)}>headless</strong>
+                : <span className={styles.gradientSlates}>h______s</span>}
+            </span>
+          </p>
+          <div className={styles.buttons}>
+            <Link
+              className="button button--primary button--lg"
+              to="/docs/intro">
+              Get Started
+            </Link>
+            <Link
+              className="button button--primary button--outline button--lg"
+              to="/docs/intro">
+              GitHub
+            </Link>
+          </div>
         </div>
+        <img className={styles.heroImage} src="/img/assets/logo@3x.png" alt="" width="320" />
       </div>
     </header>
   )
