91cce446a428ee38ecba7f53c67336f6910fd8c8
SoYoung210 <ethdud1@gmail.com>
2022-01-02 23:46:35 +0900

    test(@h6s/calendar): migrate cypress to playwright

---
+++ packages/calendar/playwright.config.ts
@@ -0,0 +1,60 @@
+import { devices, PlaywrightTestConfig } from '@playwright/test'
+
+/**
+ * https://playwright.dev/docs/test-configuration.
+ */
+const config: PlaywrightTestConfig = {
+  testDir: './playwright',
+
+  timeout: 30 * 1000,
+  expect: {
+    timeout: 5000,
+  },
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: process.env.CI ? 'dot' : 'list',
+
+  use: {
+
+    actionTimeout: 0,
+
+    /* Base URL to use in actions like `await page.goto('/')`. */
+    // baseURL: 'http://localhost:3000',
+
+    trace: 'on-first-retry',
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+
+      /* Project-specific settings. */
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox'],
+      },
+    },
+
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+      },
+    },
+  ],
+
+  /* TODO: Run local dev server before starting the tests */
+  // webServer: {
+  //   command: 'yarn start',
+  //   port: 3000,
+  // },
+}
+export default config
---
+++ packages/calendar/playwright/basic.spec.ts
@@ -0,0 +1,87 @@
+import { expect, test } from '@playwright/test'
+import {
+  addMonths,
+  format,
+  getDate,
+  getWeeksInMonth,
+  subMonths,
+} from 'date-fns'
+
+const BASIC_EXAMPLES_URL =
+  'https://calendar.h6s.dev/'
+
+const SELECTOR = {
+  CURSOR_DATE: 'data-testid=cursor-date',
+  WEEKS: 'data-testid=calendar-weeks',
+  CELL_TODAY: 'data-testid=calendar-cell--today',
+}
+
+const baseDate = new Date()
+const baseDay = getDate(baseDate)
+const baseCalendarInfo = {
+  weeksLength: getWeeksInMonth(baseDate),
+}
+
+function dateFormat(date: Date) {
+  return format(date, 'yyyy. MM')
+}
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(BASIC_EXAMPLES_URL)
+})
+
+test('render calendar successfully', async ({ page }) => {
+  // render today in title
+  const pageTitle = page.locator(SELECTOR.CURSOR_DATE)
+  await expect(pageTitle).toHaveText(dateFormat(baseDate))
+
+  // render weekends in calendar
+  const weekendsCount = await page.locator('data-testid=calendar-weekends').count()
+  expect(weekendsCount).toBe(7)
+
+  // render calendar cells
+  const weeksCount = await page.locator(SELECTOR.WEEKS).count()
+  expect(weeksCount).toBe(baseCalendarInfo.weeksLength)
+
+  // render today
+  const today = page.locator(SELECTOR.CELL_TODAY)
+  await expect(today).toHaveText(baseDay.toString())
+})
+
+test('run navigate successfully', async ({ page }) => {
+  // once prev
+  await page.click('[aria-label="button for navigating to prev calendar"]')
+  const prevTitle = page.locator(SELECTOR.CURSOR_DATE)
+  await expect(prevTitle).toHaveText(dateFormat(subMonths(baseDate, 1)))
+
+  // reset
+  await page.click('[aria-label="button for navigating to today calendar"]')
+  const resetTitle = page.locator(SELECTOR.CURSOR_DATE)
+  await expect(resetTitle).toHaveText(dateFormat(baseDate))
+
+  // once next
+  await page.click('[aria-label="button for navigating to next calendar"]')
+  const nextTitle = page.locator(SELECTOR.CURSOR_DATE)
+  await expect(nextTitle).toHaveText(dateFormat(addMonths(baseDate, 1)))
+
+  // reset again
+  // reset
+  await page.click('[aria-label="button for navigating to today calendar"]')
+  const resetTitle2 = page.locator(SELECTOR.CURSOR_DATE)
+  await expect(resetTitle2).toHaveText(dateFormat(baseDate))
+})
+
+test('run change view type successfully', async ({ page }) => {
+  await page.click('[aria-label="button for changing view type to month"]')
+
+  await page.click('[aria-label="button for changing view type to week"]')
+  const weeksCountWhenViewWeekType = await page.locator(SELECTOR.WEEKS).count()
+  expect(weeksCountWhenViewWeekType).toBe(1)
+
+  await page.click('[aria-label="button for changing view type to day"]')
+  const weeksCountWhenViewDayType = await page.locator(SELECTOR.WEEKS).count()
+  expect(weeksCountWhenViewDayType).toBe(1)
+
+  const today = page.locator(SELECTOR.CELL_TODAY)
+  await expect(today).toHaveText(baseDay.toString())
+})
