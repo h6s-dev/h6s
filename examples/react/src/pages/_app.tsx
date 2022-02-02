c826a83221aac51425333b01aa8e1c103ff0f201
JaeYeopHan <ljyhanll@gmail.com>
2022-02-03 01:31:24 +0900

    docs: arrange code

--- examples/react/src/pages/_app.tsx
+++ examples/react/src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
-import React from 'react'
 
 import theme from '../theme'
 
--- examples/react/src/pages/_document.tsx
+++ examples/react/src/pages/_document.tsx
@@ -7,7 +7,6 @@ export default class Document extends NextDocument {
       <Html>
         <Head />
         <body>
-          {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
           <Main />
           <NextScript />
