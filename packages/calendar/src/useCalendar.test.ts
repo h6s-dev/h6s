19de3fa95049a93e44bc0f5d6537037ba62ce478
JaeYeopHan <ljyhanll@gmail.com>
2023-12-12 12:29:42 +0900

    fix: add missing import statement

--- packages/calendar/src/useCalendar.test.ts
+++ packages/calendar/src/useCalendar.test.ts
@@ -1,4 +1,5 @@
 import { act, renderHook } from '@testing-library/react-hooks'
+import { describe, expect, it } from 'vitest'
 
 import { DecemberFirstWeekData, DecemberMonthMatrix } from './mocks/mockDate'
 import CalendarViewType from './models/CalendarViewType'
